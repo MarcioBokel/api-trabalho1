b77c7714803030fd0e95babb11bf15d01626fe09
MarcioBokel <marciobokel@gmail.com>
2024-05-14 15:49:41 -0300

    Primeiro commit API Trabalho 1

---
+++ src/main/java/br/org/serratec/editora/controller/LivroController.java
@@ -0,0 +1,65 @@
+package br.org.serratec.editora.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.org.serratec.editora.dto.LivroDto;
+import br.org.serratec.editora.service.LivroService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+	@Autowired
+	private LivroService livroService;
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDto>> obterTodos(){
+		return ResponseEntity.ok(livroService.obterTodos());
+	}
+	
+	@PostMapping
+	public ResponseEntity<LivroDto> salvarLivro(@Valid @RequestBody LivroDto novoLivro){
+		return new ResponseEntity<LivroDto>(livroService.salvarLivro(novoLivro), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<LivroDto> atualizarLivro(@PathVariable Long id, @Valid @RequestBody LivroDto livroAlterado) {
+		Optional<LivroDto> livro = livroService.atualizarLivro(id, livroAlterado);
+		
+		if (livro.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(livro.get());
+	}
+
+	@GetMapping("/{id}")
+    public ResponseEntity<LivroDto> obterLivroPorId(@PathVariable Long id) {
+        Optional<LivroDto> livro = livroService.obterLivroPorId(id);
+
+        if (livro.isPresent()) {
+            return ResponseEntity.ok(livro.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if(livroService.excluirLivro(id)){
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+}
---
+++ src/main/java/br/org/serratec/editora/service/LivroService.java
@@ -0,0 +1,62 @@
+package br.org.serratec.editora.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.org.serratec.editora.dto.LivroDto;
+import br.org.serratec.editora.model.Livro;
+import br.org.serratec.editora.repository.LivroRepository;
+
+@Service
+public class LivroService {
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public List<LivroDto> obterTodos(){
+		List<LivroDto> livros = new ArrayList<>();
+		livroRepository.findAll().forEach(livro -> {
+			livros.add(livro.toDto());
+			
+		});
+		return livros;
+	}
+	
+	public LivroDto salvarLivro(LivroDto novoLivro) {
+		// Aqui aconteceriam outras validações de conteúdo
+		Livro livroSalvo = livroRepository.save(novoLivro.toEntity());
+		return livroSalvo.toDto();
+	}
+
+	public Optional<LivroDto> atualizarLivro(Long id, LivroDto livroAlterado) {
+		Livro livroEntity = livroAlterado.toEntity();
+		
+		if (livroRepository.existsById(id)) {
+			livroEntity.setId(id);
+			livroRepository.save(livroEntity);
+			return Optional.of(livroEntity.toDto());
+		}
+		return Optional.empty();
+	}
+	
+	public Optional<LivroDto> obterLivroPorId(Long id) {
+        Optional<Livro> livro = livroRepository.findById(id);
+
+        if (livro.isPresent()) {
+            return Optional.of(livro.get().toDto());
+        }
+
+        return Optional.empty();
+    }
+	
+	public boolean excluirLivro(Long id) {
+		if(livroRepository.existsById(id)){
+			livroRepository.deleteById(id);
+			return true;
+		}
+		
+		return false;
+	}
+}
